theodor96/tor

commit dcabf801e52a83e2c3cc23ccc1fa906582a927d6

sched: Ignore closed channel after flushing cells
The flush cells process can close a channel if the connection write fails but
still return that it flushed at least one cell. This is due because the error
is not propagated up the call stack so there is no way of knowing if the flush
actually was successful or not.

Because this would require an important refactoring touching multiple
subsystems, this patch is a bandaid to avoid the KIST scheduler to handle
closed channel in its loop.

Bandaid on #23751.

Signed-off-by: David Goulet <dgoulet@torproject.org>

src/or/scheduler_kist.c
@@ -598,6 +598,15 @@ kist_scheduler_run(void)
     if (socket_can_write(&socket_table, chan)) {
       /* flush to channel queue/outbuf */
       flush_result = (int)channel_flush_some_cells(chan, 1); // 1 for num cells
+      /* XXX: While flushing cells, it is possible that the connection write
+       * fails leading to the channel to be closed which triggers a release
+       * and free its entry in the socket table. And because of a engineering
+       * design issue, the error is not propagated back so we don't get an
+       * error at this poin. So before we continue, make sure the channel is
+       * open and if not just ignore it. See #23751. */
+      if (!CHANNEL_IS_OPEN(chan)) {
+        continue;
+      }
       /* flush_result has the # cells flushed */
       if (flush_result > 0) {
         update_socket_written(&socket_table, chan, flush_result *

commit 2374e81d70b08f2c79a65914e8224977270144a4

sched: Set channel scheduler state to IDLE when not opened
In the KIST main loop, if the channel happens to be not opened, set its state
to IDLE so we can release it properly later on. Prior to this fix, the channel
was in PENDING state, removed from the channel pending list and then kept in
that state because it is not opened.

This bug was introduced in commit dcabf801e52a83e2c3cc23ccc1fa906582a927d6 for
which we made the scheduler loop not consider unopened channel.

This has no consequences on tor except for an annoying but harmless BUG()
warning.

Fixes #24502

Signed-off-by: David Goulet <dgoulet@torproject.org>

src/or/scheduler_kist.c
@@ -606,9 +606,12 @@ kist_scheduler_run(void)
        * fails leading to the channel to be closed which triggers a release
        * and free its entry in the socket table. And because of a engineering
        * design issue, the error is not propagated back so we don't get an
-       * error at this poin. So before we continue, make sure the channel is
+       * error at this point. So before we continue, make sure the channel is
        * open and if not just ignore it. See #23751. */
       if (!CHANNEL_IS_OPEN(chan)) {
+        /* Channel isn't open so we put it back in IDLE mode. It is either
+         * renegotiating its TLS session or about to be released. */
+        chan->scheduler_state = SCHED_CHAN_IDLE;
         continue;
       }
       /* flush_result has the # cells flushed */